df503c9c62f1a5e4751da52900b43b1156b1803c
LeifCastle <leifurcastle@gmail.com>
2023-09-14 12:11:08 -0700

    FEAT: added project details and techstack to Project Description component and populated with data

--- src/app/components/Projects/ProjectDescription.js
+++ src/app/components/Projects/ProjectDescription.js
@@ -1,4 +1,14 @@
+import Skill from "../Skill";
+
 export default function ProjectDescription({ project }) {
+  let details = project.details.map((detail) => {
+    return <li>{detail}</li>;
+  });
+
+  let skills = project.techStack.map((skill) => {
+    return <Skill skill={skill} key={skill} />;
+  });
+
   return (
     <div className="flex flex-col bg-button1 rounded-md w-[60vw] h-[70vh] overflow-y-scroll no-scrollbar">
       <div className="flex flex-col items-center">
@@ -8,8 +18,11 @@ export default function ProjectDescription({ project }) {
         <hr className="w-[20vw] mt-1 mb-4 border-[3px] border-slate-200 rounded-full opacity-25"></hr>
       </div>
       <div className="mx-4">
-        <p className="">{project.overview}</p>
-        <p className="">{project.techStack}</p>
+        <p className="text-grayText">{project.overview}</p>
+        <div className="flex m-4">
+          <ul className="list-disc text-grayText">{details}</ul>
+        </div>
+        <div className="flex flex-wrap">{skills}</div>
       </div>
     </div>
   );
